f9440a7f60dd02862bf50f6d06b14033a430049a
Ashley <ashleyjackson212@yahoo.com>
2025-03-20 16:43:57 -0500

    Fixed footer image issue and updated image file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import RealEstateCalculator from "./RealEstateCalculator";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
